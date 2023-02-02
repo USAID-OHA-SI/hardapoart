098676b9d22b260d157ae18a4721d0f3cc24cbcd
Aaron Chafetz <achafetz@usaid.gov>
2023-02-02 17:37:46 -0500

    feat: facet draft

--- Scripts/13_funding-distro_funding-flavors.R
+++ Scripts/13_funding-distro_funding-flavors.R
@@ -22,9 +22,11 @@
   
   ref_id <- "e258e5d3" #id for adorning to plots, making it easier to find on GH
   
-  get_metadata() #list of MSD metadata elements
+  get_metadata(type = "Financial") #list of MSD metadata elements
 
   load_secrets("email")
+  
+  cntry <- "Malawi"
   
 # IMPORT ------------------------------------------------------------------
   
@@ -35,16 +37,46 @@
 
 # MUNGE -------------------------------------------------------------------
 
+  #limit to select country
+  df_fsd <- df_fsd %>% 
+    filter(country == cntry,
+           expenditure_amt != 0)
+  
+  #assign funding type
+  df_fsd <- df_fsd %>% 
+    mutate(funding_type = case_when(interaction_type %in% c("Not Specified", "Service Delivery") ~ interaction_type,
+                                    program == "ASP" ~ "Above Site NSD",
+                                    program %in% c("PM", "Applied Pipeline") ~ program,
+                                    interaction_type == "Non Service Delivery" ~ "Site Level NSD")) 
+  
   #remove M&O and supply chain
   df_fsd <- df_fsd %>% 
     remove_mo() %>% 
     remove_sch()
+  
+  #aggregate by year and 
+  df_fsd_agg <- df_fsd %>% 
+    mutate(agency = ifelse(funding_agency == "USAID", "USAID", "Other")) %>% 
+    count(fiscal_year, agency, funding_type, wt = expenditure_amt, name = "expenditure_amt") %>% 
+    pivot_wider(names_from = agency, values_from = expenditure_amt,
+                names_glue = "{tolower(agency)}") %>% 
+    rowwise() %>% 
+    mutate(pepfar = sum(usaid, other, na.rm = TRUE)) %>% 
+    ungroup() %>% 
+    group_by(fiscal_year) %>% 
+    mutate(usaid_total = sum(usaid, na.rm = TRUE)) %>% 
+    ungroup()
+  
 
+# VIZ ---------------------------------------------------------------------
 
-  #assign funding type
-    df_fsd %>% 
-      mutate(funding_type = case_when(interaction_type %in% c("Not Specified", "Service Delivery") ~ interaction_type,
-                                    program == "ASP" ~ "Above Site NSD",
-                                    program %in% c("PM", "Applied Pipeline") ~ program,
-                                    interaction_type == "Non Service Delivery" ~ "Site Level NSD")) 
-
+  
+  df_fsd_agg %>% 
+    ggplot(aes(fiscal_year, usaid, group = funding_type, fill = funding_type)) +
+    geom_area(aes(y = usaid_total), fill = "#909090", alpha = .2) +
+    geom_area() +
+    facet_grid(~fct_reorder2(funding_type, fiscal_year, usaid)) +
+    scale_y_continuous(label = number_format(scale = 1e-6, prefix = "$", suffix = "M")) + 
+    labs(x = NULL, y = NULL,
+         caption = glue("{metadata$caption}")) +
+    si_style_ygrid()

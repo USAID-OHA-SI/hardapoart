bb80b4990c8edd7ef6db550d5b63854adbd3b1cc
jhoehner <jhoehner@usaid.gov>
2023-02-07 16:14:49 -0500

    feat: WIP slide 11

--- Scripts/11_Linkage_Gaps_PSNU_priorityPops.R
+++ Scripts/11_Linkage_Gaps_PSNU_priorityPops.R
@@ -46,34 +46,66 @@
     resolve_knownissues()
   
   # Linkage Trends
-  df_hts_base <- df_msd %>% 
+  # National roll-up:
+  
+  df_hts_nat_base <- df_msd %>% 
     filter(indicator %in% c("HTS_TST_POS", "TX_NEW", "HTS_TST"), 
            standardizeddisaggregate == "Total Numerator",
-           fiscal_year <= metadata$curr_fy, 
+           fiscal_year == metadata$curr_fy,
            funding_agency == "USAID") %>% 
     group_by(indicator, fiscal_year) %>% 
+    summarise(across(targets:qtr4, \(x) sum(x, na.rm = T)), 
+              .groups = "drop") %>% 
+    reshape_msd(direction ="semi-wide") %>% 
+    group_by(indicator) %>% 
+    fill(targets, .direction = "down") %>% 
+    filter(nchar(period) != 4, 
+           period == metadata$curr_pd) 
+  
+  df_hts_tgt_nat <- df_hts_nat_base %>% 
+    select(-results) %>% 
+    pivot_wider(names_from = indicator, values_from = targets) %>% 
+    mutate(linkage = TX_NEW / HTS_TST_POS)
+  
+  df_linkage_nat <- df_hts_nat_base %>% 
+    select(-targets) %>% 
+    pivot_wider(names_from = indicator, values_from = results) %>% 
+    mutate(linkage = TX_NEW / HTS_TST_POS)
+  
+  # PSNU
+  df_hts_psnu_base <- df_msd %>% 
+    filter(indicator %in% c("HTS_TST_POS", "TX_NEW", "HTS_TST"), 
+           standardizeddisaggregate == "Total Numerator",
+           fiscal_year == metadata$curr_fy,
+           funding_agency == "USAID") %>% 
+    group_by(indicator, psnu, fiscal_year) %>% 
     summarise(across(targets:qtr4, \(x) sum(x, na.rm = T)), 
                      .groups = "drop") %>% 
     reshape_msd(direction ="semi-wide") %>% 
     group_by(indicator) %>% 
     fill(targets, .direction = "down") %>% 
-    filter(nchar(period) != 4) 
+    filter(nchar(period) != 4, 
+           period == metadata$curr_pd) 
   
-  df_hts_tgt <- df_hts_base %>% 
+  df_hts_tgt_psnu <- df_hts_psnu_base %>% 
     select(-results) %>% 
     pivot_wider(names_from = indicator, values_from = targets) %>% 
     mutate(linkage = TX_NEW / HTS_TST_POS)
   
-  df_linkage <- df_hts_base %>% 
+  df_linkage_psnu <- df_hts_psnu_base %>% 
     select(-targets) %>% 
     pivot_wider(names_from = indicator, values_from = results) %>% 
     mutate(linkage = TX_NEW / HTS_TST_POS)
+  
+  df_linkage <- bind_rows(df_linkage_nat, df_linkage_psnu)
+  
+  
   
   # VIZ ===========================================================================
   
   # Linkage visual w/ targets
   bottom_hts <- df_linkage %>% 
-    ggplot(aes(x = period)) +
+    ggplot(aes(x = reorder(psnu, HTS_TST))) +
     # geom_col(data = df_hts_tgt, aes(y = HTS_TST_POS), fill = "#efe9ed",
     #          position = position_nudge(x = 0.15), width = 0.5) +
     # geom_col(data = df_hts_tgt, aes(y = TX_NEW), fill = "#f8ead9", 
@@ -83,14 +115,15 @@
     geom_col(aes(y = TX_NEW), fill = "#D9AF6B", 
              position = position_nudge(x = -0.1), width = 0.5) +
     si_style_ygrid() +
+    coord_flip() +
     scale_y_continuous(labels = comma) +
     labs(x = NULL, y = NULL) +
     expand_limits(x = c(0, 9)) 
   
   # Linkage plot - #b08472
   top_hts <- df_linkage %>% 
-    ggplot(aes(x = period, group = 1)) +
-    geom_line(aes(y = linkage), color = grey50k, size = 0.5) +
+    ggplot(aes(x = psnu, group = 1)) +
+    geom_line(aes(y = linkage), color = grey50k, linewidth = 0.5) +
     geom_point(aes(y = linkage), shape = 19, color = "#b08472",  size = 3) + 
     geom_point(aes(y = linkage), shape = 1, color = grey90k,  size = 3) + 
     geom_text(aes(y = linkage, label = percent(linkage, 1)), 
@@ -105,11 +138,10 @@
           axis.text.x = element_blank()) +
     labs(x = NULL, y = NULL) +
     annotate("text", x = 8.5, y = 0.87, label = "Linkage", 
-             linewidth = 11/.pt, color = "#b08472")
+             size = 11/.pt, color = "#b08472")
   
   top_hts / bottom_hts +
     plot_layout(heights = c(1, 4))
-  
   
   si_save("Graphics/Linkage_summary.svg")  
   

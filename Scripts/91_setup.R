572265057348ee92e9eb6d3a7a32ff71eb91fe42
Aaron Chafetz <achafetz@usaid.gov>
2023-02-10 13:23:34 -0500

    feat: duplicate data frame to create a PEPFAR total

--- Scripts/91_setup.R
+++ Scripts/91_setup.R
@@ -104,6 +104,10 @@
     df_msd <- df_msd %>% 
       semi_join(df_msd_ind, by = c("indicator", "standardizeddisaggregate"))
     
+    #add in PEPFAR "agency"
+    df_msd <- df_msd %>% 
+      bind_rows(df_msd %>% mutate(funding_agency = "PEPFAR"))
+    
     #resolve known issues
     df_msd <- resolve_knownissues(df_msd)
     

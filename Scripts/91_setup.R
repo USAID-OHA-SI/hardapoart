6031c265f0d5e548710d7ff771c2e4fa48d7be65
Aaron Chafetz <achafetz@usaid.gov>
2023-02-10 08:54:45 -0500

    feat: create dummy datasets if not stored locally

--- Scripts/91_setup.R
+++ Scripts/91_setup.R
@@ -40,68 +40,89 @@
 
 # LOAD MSD ----------------------------------------------------------------
 
-  #store meta data
-  get_metadata(type = "PSNU_IM")
-  metadata_msd <- metadata
-  rm(metadata)
   
-  #import
-  df_msd <- si_path() %>% 
-    return_latest("PSNU_IM") %>% 
-    read_psd()   
+  if(!file.exists(return_latest(si_path(), "PSNU_IM"))){
+    df_msd <- tibble::tibble(fiscal_year = NA,
+                             country = NA,
+                             fundingagency = NA)
+  } else {
+    
+    #store meta data
+    get_metadata(type = "PSNU_IM")
+    metadata_msd <- metadata
+    rm(metadata)
+    
+    #import
+    df_msd <- si_path() %>% 
+      return_latest("PSNU_IM") %>% 
+      read_psd()   
+    
+    #resolve known issues
+    df_msd <- resolve_knownissues(df_msd)
+    
+    #filter to data from last 5 quarters & relevant indicators/disaggs
+    df_msd <- df_msd %>% 
+      filter(fiscal_year >= 2022)
+    
+    
+    #add _D to denom variables
+    df_msd <- clean_indicator(df_msd)
+    
+    #MSD filter table
+    df_msd_ind <- tibble::tribble(
+      ~indicator,      ~standardizeddisaggregate,
+      "HTS_TST",              "Total Numerator",
+      "HTS_TST_POS",              "Total Numerator",
+      "KP_PREV",              "Total Numerator",
+      "OVC_SERV",              "Total Numerator",
+      "PMTCT_EID",              "Total Numerator",
+      "PrEP_NEW",                      "Age/Sex",
+      "PrEP_NEW",                    "KeyPopAbr",
+      "PrEP_NEW",              "Total Numerator",
+      "TB_PREV",              "Total Numerator",
+      "TX_CURR",            "Age/Sex/HIVStatus",
+      "TX_CURR",              "Total Numerator",
+      "TX_NEW",              "Total Numerator",
+      "TX_PVLS", "Age/Sex/Indication/HIVStatus",
+      "TX_PVLS",              "Total Numerator",
+      "TX_PVLS_D",            "Total Denominator",
+      "VMMC_CIRC",              "Total Numerator"
+    )
+    
+    #filter to select indicators/disaggs
+    df_msd <- df_msd %>% 
+      semi_join(df_msd_ind, by = c("indicator", "standardizeddisaggregate"))
+  }
   
-  #resolve known issues
-  df_msd <- resolve_knownissues(df_msd)
-  
-  #filter to data from last 5 quarters & relevant indicators/disaggs
-  df_msd <- df_msd %>% 
-    filter(fiscal_year >= 2022)
-  
-  
-  #add _D to denom variables
-  df_msd <- clean_indicator(df_msd)
-  
-  #MSD filter table
-  df_msd_ind <- tibble::tribble(
-    ~indicator,      ~standardizeddisaggregate,
-    "HTS_TST",              "Total Numerator",
-    "HTS_TST_POS",              "Total Numerator",
-    "KP_PREV",              "Total Numerator",
-    "OVC_SERV",              "Total Numerator",
-    "PMTCT_EID",              "Total Numerator",
-    "PrEP_NEW",                      "Age/Sex",
-    "PrEP_NEW",                    "KeyPopAbr",
-    "PrEP_NEW",              "Total Numerator",
-    "TB_PREV",              "Total Numerator",
-    "TX_CURR",            "Age/Sex/HIVStatus",
-    "TX_CURR",              "Total Numerator",
-    "TX_NEW",              "Total Numerator",
-    "TX_PVLS", "Age/Sex/Indication/HIVStatus",
-    "TX_PVLS",              "Total Numerator",
-    "TX_PVLS_D",            "Total Denominator",
-    "VMMC_CIRC",              "Total Numerator"
-  )
-
-  #filter to select indicators/disaggs
-  df_msd <- df_msd %>% 
-    semi_join(df_msd_ind, by = c("indicator", "standardizeddisaggregate"))
   
   
 
 # LOAD FSD ----------------------------------------------------------------
 
-  #store meta data
-  get_metadata(type = "Financial")
-  metadata_fsd <- metadata
-  rm(metadata)
-  
-  #import
-  df_fsd <- si_path() %>% 
-    return_latest("Financial") %>% 
-    read_psd()   
+  if(!file.exists(return_latest(si_path(), "Financial"))){
+    df_msd <- tibble::tibble(fiscal_year = NA,
+                             country = NA,
+                             fundingagency = NA)
+  } else {
+    
+    #store meta data
+    get_metadata(type = "Financial")
+    metadata_fsd <- metadata
+    rm(metadata)
+    
+    #import
+    df_fsd <- si_path() %>% 
+      return_latest("Financial") %>% 
+      read_psd() 
+    
+  }
   
 # LOAD NAT_SUBNAT ---------------------------------------------------------
 
+  if(!file.exists(return_latest(si_path(), "NAT_SUBNAT"))){
+    df_msd <- tibble::tibble(fiscal_year = NA,
+                             country = NA)
+  } else {
   #store meta data
   get_metadata(type = "NAT_SUBNAT")
   metadata_natsubnat <- metadata
@@ -121,9 +142,15 @@
   #filter to select indicators/disaggs
   df_natsubnat <- df_natsubnat %>% 
     semi_join(df_natsubnat_ind, by = c("indicator", "standardizeddisaggregate"))
+  }
 
 # LOAD HRH ----------------------------------------------------------------
 
+  if(!file.exists(return_latest(si_path(), "HRH"))){
+    df_msd <- tibble::tibble(fiscal_year = NA,
+                             country = NA,
+                             fundingagency = NA)
+  } else {
   #store meta data
   get_metadata(type = "HRH")
   metadata_hrh <- metadata
@@ -137,6 +164,7 @@
   #limit to latest fy
   df_hrh <- df_hrh %>% 
     filter(fiscal_year == max(fiscal_year, na.rm = TRUE))
+  }
 
 # LOAD SID ----------------------------------------------------------------
 
@@ -173,24 +201,24 @@
   metadata_tens <- list(caption = "Source: HIV Policy Lab [2021-11-09]")
   
   #read in HIV Policy Lab data export
-  df_tens <- googlesheets4::range_speedread(pol_lab_id, "Policy adoption data", skip = 6,
-                                            col_types = "c") %>% 
+  df_tens <- googlesheets4::range_speedread(pol_lab_id, "Policy adoption data", 
+                                            skip = 6, col_types = "c") %>% 
     janitor::clean_names()
   
 
 # MARKDOWN ----------------------------------------------------------------
 
-  #TODO
-  vct_cntry <- pepfar_country_list %>% 
-    pull(country)
-  
-  #output files
-  reports <- tibble(
-    output_file = glue(here("markdown","{curr_pd}_{vct_cntry}_cop-support-viz_oha-siei.pptx")),
-    params = map(vct_cntry, ~list(vct_cntry = .))
-  )
-  
-  #create reports
-  reports %>%
-    pwalk(render, 
-          input = here("Scripts","reports.Qmd"))
+  # #TODO
+  # vct_cntry <- pepfar_country_list %>% 
+  #   pull(country)
+  # 
+  # #output files
+  # reports <- tibble(
+  #   output_file = glue(here("markdown","{curr_pd}_{vct_cntry}_cop-support-viz_oha-siei.pptx")),
+  #   params = map(vct_cntry, ~list(vct_cntry = .))
+  # )
+  # 
+  # #create reports
+  # reports %>%
+  #   pwalk(render, 
+  #         input = here("Scripts","reports.Qmd"))

98ece1e789f89ea3886c688f17635e33f022226a
Baboyma Kagniniwa <baboyma@users.noreply.github.com>
2023-02-10 14:21:24 -0500

    VLCS by Age Group

--- Scripts/08_vl-coverage-and-suppression_gp-age.R
+++ Scripts/08_vl-coverage-and-suppression_gp-age.R
@@ -133,31 +133,30 @@
   #' @title Viz TX VL
   #' 
   viz_viral_load <- function(df, cntry, pd, src, rid,
-                             disagg = c("age", "psnu"),
-                             save = T) {
-    
-    .disagg <- match.arg(disagg)
+                             save = F) {
     
     viz <- df %>% 
       ggplot(aes(x = period, group = 1)) +
       geom_line(aes(y = vlc), color = burnt_sienna, linewidth = 1) +
       geom_point(aes(y = vlc), fill = burnt_sienna, color = grey10k, shape = 21, size = 4) +
-      geom_text(aes(y = vlc, label = percent(vlc, 1)), vjust = 1.5, color = burnt_sienna) +
+      geom_text(aes(y = vlc, label = percent(vlc, 1)), vjust = 2, color = burnt_sienna) +
       geom_line(aes(y = vls), color = genoa, linewidth = 1) +
       geom_point(aes(y = vls), fill = genoa, color = grey10k, shape = 21, size = 4) +
       geom_text(aes(y = vls, label = percent(vls, 1)), vjust = -1.8, color = genoa) +
-      #geom_text(aes(y = vls, label = percent(vls, 1)), vjust = "inward", hjust = "inward", color = genoa) +
       scale_y_continuous(labels = percent) +
       labs(x = "", y = "",
-           title = glue::glue("{toupper(cntry)} - VIRAL LOAD TRENDS"),
+           title = glue::glue("{toupper(agency)}/{toupper(cntry)} - VIRAL LOAD TRENDS"),
            subtitle = glue::glue("<span style='color:{burnt_sienna}'>Coverage</span> & <span style='color:{genoa}'>Supression</span>"),
            caption = glue::glue("Source: {src} - Created by OHA/SIEI | Ref. ID #{rid}")) +
-      facet_wrap(~!!.disagg) +
+      coord_cartesian(clip = "off") +
+      facet_wrap(~age) +
       si_style_nolines() +
       theme(plot.title = element_markdown(),
             plot.subtitle = element_markdown(),
             axis.text.y = element_blank(),
             strip.text = element_text(size = 12, face = "bold"),
+            strip.clip = "off",
+            strip.placement = "outside",
             panel.spacing = unit(0, "lines"))
     
     print(viz)
@@ -186,140 +185,41 @@
                               cntry = cntry,
                               pd_hist = 5)
   
-  # ## TX Indicators ----
-  # 
-  # df_tx <- df_psnu %>% 
-  #   filter(
-  #     fiscal_year %in% c(curr_fy, curr_fy - 1),
-  #     funding_agency != "Dedup", 
-  #     indicator %in% inds_vl,
-  #     standardizeddisaggregate %in% disaggs_vl
-  #   ) %>% 
-  #   mutate(indicator = case_when(
-  #       indicator == "TX_PVLS" & numeratordenom == "D" ~ paste0(indicator, "_D"),
-  #       TRUE ~ indicator)
-  #   ) 
-  # 
-  # 
-  # 
-  # df_tx <- df_tx %>%
-  #   select(-cumulative, -targets) %>% 
-  #   filter(ageasentered != "Unknown Age") %>% 
-  #   mutate(age = case_when(
-  #     trendscoarse == "<15" ~ trendscoarse,
-  #     ageasentered == "15-19" ~ ageasentered,
-  #     ageasentered %in% "20-24" ~ ageasentered,
-  #     TRUE ~ "25+"
-  #   )) %>% 
-  #   group_by(fiscal_year, funding_agency, operatingunit, indicator, age) %>%
-  #   #summarise(across(starts_with("qtr"), sum, na.rm = TRUE), .groups = "drop") %>%
-  #   #summarise(across(where(is.numeric), sum, na.rm = TRUE), .groups = "drop") %>%
-  #   summarise(qtr1 = sum(qtr1, na.rm = TRUE), 
-  #             qtr2 = sum(qtr2, na.rm = TRUE),
-  #             qtr3 = sum(qtr3, na.rm = TRUE),
-  #             qtr4 = sum(qtr4, na.rm = TRUE),
-  #             .groups = "drop") 
-  # 
-  # df_tx <- df_tx %>% reshape_msd()
-  # 
-  # 
-  # ## VL Calculation
-  # 
-  # df_vl <- df_tx %>% 
-  #   select(-period_type) %>% 
-  #   pivot_wider(names_from = indicator, values_from = value) %>% 
-  #   rename_with(str_to_lower) %>% 
-  #   group_by(funding_agency, operatingunit, age) %>% 
-  #   mutate(
-  #     vlc = tx_pvls_d / dplyr::lag(tx_curr, 2, order_by = period),
-  #     vls = tx_pvls / tx_pvls_d
-  #   ) %>% 
-  #   ungroup()
-  # 
-  # ## Limits history to last 5
-  # 
-  # hist_pds <- df_vl %>% 
-  #   distinct(period) %>% 
-  #   arrange(desc(period)) %>% 
-  #   pull() %>% 
-  #   magrittr::extract(1:5) %>% 
-  #   sort()
-  # 
-  # df_vl <- df_vl %>% filter(period %in% hist_pds)
-  
     
 # VIZ ----
+  
+  # Agency
+  prep_varial_load(df = df_psnu, 
+                   fy = metadata_msd$curr_fy, 
+                   agency = agency, 
+                   cntry = cntry,
+                   pd_hist = 5) %>% 
+    viz_viral_load(
+      df = ., 
+      cntry = cntry, 
+      pd = metadata_msd$curr_pd, 
+      src = metadata_msd$source,
+      rid = ref_id,
+      save = F
+    )
+  
+  # All
+  agency <- "PEPFAR"
+  
+  prep_varial_load(df = df_psnu, 
+                   fy = metadata_msd$curr_fy, 
+                   agency = agency, 
+                   cntry = cntry,
+                   pd_hist = 5) %>% 
+    viz_viral_load(
+      df = ., 
+      cntry = cntry, 
+      pd = metadata_msd$curr_pd, 
+      src = metadata_msd$source,
+      rid = ref_id,
+      save = F
+    )
 
-  
-  viz_viral_load(
-    df = df_vlcs, 
-    cntry = cntry, 
-    pd = metadata_msd$curr_pd, 
-    src = metadata_msd$source,
-    rid = ref_id,
-    disagg = c("age", "psnu"),
-    save = T
-  )
-  
-  df_vlcs %>% 
-    ggplot(aes(x = period, group = 1)) +
-    geom_line(aes(y = vlc), color = burnt_sienna, linewidth = 1) +
-    geom_point(aes(y = vlc), fill = burnt_sienna, color = grey10k, shape = 21, size = 4) +
-    geom_text(aes(y = vlc, label = percent(vlc, 1)), vjust = 1.5, color = burnt_sienna) +
-    geom_line(aes(y = vls), color = genoa, linewidth = 1) +
-    geom_point(aes(y = vls), fill = genoa, color = grey10k, shape = 21, size = 4) +
-    geom_text(aes(y = vls, label = percent(vls, 1)), vjust = -1.8, color = genoa) +
-    #geom_text(aes(y = vls, label = percent(vls, 1)), vjust = "inward", hjust = "inward", color = genoa) +
-    scale_y_continuous(labels = percent) +
-    labs(x = "", y = "",
-         title = glue::glue("{toupper(cntry)} - VIRAL LOAD TRENDS"),
-         subtitle = glue::glue("<span style='color:{burnt_sienna}'>Coverage</span> & <span style='color:{genoa}'>Supression</span>"),
-         caption = glue::glue("Source: {metadata_msd$source} - Created by OHA/SIEI | Ref. ID #{ref_id}")) +
-    facet_wrap(~age) +
-    si_style_nolines() +
-    theme(plot.title = element_markdown(),
-          plot.subtitle = element_markdown(),
-          axis.text.y = element_blank(),
-          strip.text = element_text(size = 12, face = "bold"),
-          panel.spacing = unit(0, "lines"))
-  
-  
-  
-  ## ----
-  df_vl %>% 
-    filter(funding_agency == agency,
-           str_detect(operatingunit, "Region$", negate = T)) %>% 
-    distinct(operatingunit) %>% 
-    pull() %>% 
-    last() %>% 
-    walk(function(.ou) {
-      viz <- df_vl %>% 
-        filter(funding_agency == agency,
-               operatingunit == .ou) %>% 
-        ggplot(aes(x = period, group = 1)) +
-        geom_line(aes(y = vlc), color = burnt_sienna, linewidth = 1) +
-        geom_point(aes(y = vlc), fill = burnt_sienna, color = grey10k, shape = 21, size = 4) +
-        geom_text(aes(y = vlc, label = percent(vlc, 1)), vjust = 2, color = burnt_sienna) +
-        geom_line(aes(y = vls), color = genoa, linewidth = 1) +
-        geom_point(aes(y = vls), fill = genoa, color = grey10k, shape = 21, size = 4) +
-        geom_text(aes(y = vls, label = percent(vls, 1)), vjust = -2, color = genoa) +
-        scale_y_continuous(labels = percent) +
-        labs(x = "", y = "",
-             title = glue::glue("{toupper(.ou)} - VIRAL LOAD TRENDS"),
-             subtitle = glue::glue("<span style='color:{burnt_sienna}'>Coverage</span> & <span style='color:{genoa}'>Supression</span>"),
-             caption = glue::glue("Source: {src_msd} - Created by OHA/SIEI | Ref. ID #{ref_id}")) +
-        facet_wrap(~age) +
-        si_style_nolines() +
-        theme(plot.title = element_markdown(),
-              plot.subtitle = element_markdown(),
-              axis.text.y = element_blank(),
-              strip.text = element_text(size = 12, face = "bold"))
-      
-      print(viz)
-      
-      si_save(plot = viz,
-              filename = glue::glue("./Graphics/{curr_pd} - {toupper(.ou)} VLCS Trends by Age Group.png"))
-    })
   
 # EXPORT ----
   

3fe79c2660dcd784c552aa9595ebbcd1e0d57a3f
Aaron Chafetz <achafetz@usaid.gov>
2023-02-13 09:14:17 -0500

    feat: adj labels for smaller categories

--- Scripts/14_hrh-footprint-various.R
+++ Scripts/14_hrh-footprint-various.R
@@ -73,9 +73,12 @@ prep_hrh_footprint <- function(df, cntry, agency){
   #create labels
   df_viz <- df_total %>% 
     dplyr::bind_rows(df_sd, df_loc, df_er) %>%
-    dplyr::mutate(lab_val = ifelse(subcat == "Total Staffing",
-                                   glue::glue("**{subcat}**<br>FTEs: {scales::number_format(accuracy = .1, scale_cut = cut_short_scale())(individual_count)}<br>Individual Staff: {scales::number_format(accuracy = .1, scale_cut = cut_short_scale())(annual_fte)} | Actual Annual Spend: {scales::number_format(accuracy = .1, prefix = '$', scale_cut = cut_short_scale())(actual_annual_spend)}"),
-                                   glue::glue("**{subcat}**<br>{scales::number_format(accuracy = .1, scale_cut = cut_short_scale())(individual_count)}<br>{scales::number_format(accuracy = .1, scale_cut = cut_short_scale())(annual_fte)} | {scales::number_format(accuracy = .1, prefix = '$', scale_cut = cut_short_scale())(actual_annual_spend)}")))
+    dplyr::group_by(cat) %>% 
+    dplyr::mutate(share = annual_fte/sum(annual_fte, na.rm = TRUE)) %>% 
+    dplyr::ungroup() %>% 
+    dplyr::mutate(lab_val = dplyr::case_when(subcat == "Total Staffing" ~ glue::glue("**{subcat}**<br>FTEs: {scales::number_format(accuracy = .1, scale_cut = cut_short_scale())(individual_count)}<br>Individual Staff: {scales::number_format(accuracy = .1, scale_cut = cut_short_scale())(annual_fte)} | Actual Annual Spend: {scales::number_format(accuracy = .1, prefix = '$', scale_cut = cut_short_scale())(actual_annual_spend)}"),
+                                   share >=.12 ~ glue::glue("**{subcat}**<br>{scales::number_format(accuracy = .1, scale_cut = cut_short_scale())(individual_count)}<br>{scales::number_format(accuracy = .1, scale_cut = cut_short_scale())(annual_fte)} | {scales::number_format(accuracy = .1, prefix = '$', scale_cut = cut_short_scale())(actual_annual_spend)}")),
+                  lab_90 = dplyr::case_when(is.na(lab_val) ~ subcat))
   
   #order categories (and number for color mapping)
   df_viz <- df_viz %>% 
@@ -118,17 +121,21 @@ viz_hrh_footprint <- function(df){
   ref_id <- "d98b536f" #id for adorning to plots, making it easier to find on GH
   
   df %>% 
-    ggplot2::ggplot(aes(individual_count, cat, fill = fill_color, group = subcat)) +
+    ggplot2::ggplot(aes(annual_fte, cat, fill = fill_color, group = subcat)) +
     ggplot2::geom_col(color = "white") +
     ggtext::geom_richtext(aes(label = lab_val, color = text_color), position = position_stack(vjust = .5),
-                           label.color = NA, fill = NA,
+                           label.color = NA, fill = NA, na.rm = TRUE,
                            family = "Source Sans Pro") +
+    ggtext::geom_richtext(aes(label = lab_90, color = text_color), angle = 90, position = position_stack(vjust = .5),
+                          label.color = NA, fill = NA, na.rm = TRUE, size = 8/.pt,
+                          family = "Source Sans Pro") +
     ggplot2::facet_wrap(.~fct_rev(cat), scales = "free", ncol = 1) +
     ggplot2::scale_fill_identity() +
     ggplot2::scale_color_identity() +
     ggplot2::labs(x = NULL, y = NULL,
                   subtitle = glue("{metadata_hrh$curr_fy_lab} HRH Staffing Footprint in {unique(df$funding_agency)}/{unique(df$country)} Broken Down By FTEs"),
                   caption = glue("{metadata_hrh$caption} Structured Dataset (not redacted) | USAID | Ref id: {ref_id}")) + 
+    ggplot2::coord_cartesian(clip = "off") +
     glitr::si_style_nolines() +
     ggplot2::theme(legend.position = "none",
                    axis.text = element_blank(),

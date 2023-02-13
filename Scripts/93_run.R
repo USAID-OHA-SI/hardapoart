d4690170335631434186461a98eb86e3f83612c3
Aaron Chafetz <achafetz@usaid.gov>
2023-02-13 10:38:53 -0500

    fix: clean up run file

--- Scripts/93_run.R
+++ Scripts/93_run.R
@@ -1,23 +1,47 @@
+# PROJECT:  hardapoart
+# AUTHOR:   A.Chafetz | USAID
+# PURPOSE:  iterate through country reports
+# REF ID:   5bea2027 
+# LICENSE:  MIT
+# DATE:     2023-02-13
+# UPDATED: 
+
+# DEPENDENCIES ------------------------------------------------------------
+
 library(tidyverse)
-library(gagglr)
 library(glue)
+library(gophr)
 library(here)
 library(quarto)
+library(rmarkdown)
 
-get_metadata(type = "PSNU_IM")
 
-vct_cntry <- pepfar_country_list %>%
-  pull(country)
+# SETUP DATA --------------------------------------------------------------
 
-vct_cntry[1:2]
+source(here("Scripts/91_setup.R"))
 
-#output files
-reports <- tibble(
-  output_file = glue(here("markdown","{metadata$curr_pd}_{vct_cntry}_cop-support-viz_oha-siei.pptx")),
-  params = map(vct_cntry, ~list(curr_pd = metadata$curr_pd, cntry = ., agency = "PEPFAR"))
-)
 
-#create reports
-reports %>%
-  pwalk(quarto_render,
-        input = here("Scripts","reports.Qmd"))
+# GLOBAL VARIABLES --------------------------------------------------------
+
+  # get_metadata()
+  curr_pd <- "FY23Q1"
+  
+  # vct_cntry <- glamr::pepfar_country_list$country
+  vct_cntry <- "Malawi"
+  
+# GENERATE REPORTS --------------------------------------------------------
+
+  #clean names for filenaming (remove space and apostrophes)
+  vct_cntry_clean <- gsub("( |\')", "", vct_cntry)
+  
+  #output files
+  reports <- tibble(
+    output_file = glue(here("markdown","{curr_pd}_{vct_cntry_clean}_cop-support-viz_oha-siei.html")),
+    params = map(vct_cntry, ~list(cntry = ., agency = "PEPFAR"))
+  )
+
+    
+  #create reports
+  reports %>%
+    pwalk(render,
+          input = here("Scripts","country_reports.Rmd"))

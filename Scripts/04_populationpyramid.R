6f5127655c4683308fc1c39673d2c6cce251051c
jhoehner <jhoehner@usaid.gov>
2023-02-23 13:06:06 -0500

    feat: incorporated feedback from AC

--- Scripts/04_populationpyramid.R
+++ Scripts/04_populationpyramid.R
@@ -13,39 +13,23 @@
   # cntry is a string
   # selected_ind is either "POP_EST" or "PLHIV"
 
-  prep_pop_pyramid <- function(df, cntry, selected_ind, ...){
-  
+  prep_pop_pyramid <- function(df, cntry, ...){
+
   # clean exit if no data
   if(cntry %ni% unique(df$country))
-    return(NULL)
-  
-  # clean exit if missing required indicators
-  assertr::verify(df, 
-                  selected_ind %in% indicator,
-                  error_fun = err_text(glue::glue("Error: {df} does not contain the required indicators PLHIV or POP_EST.
-                                               Please check {df} for at least one of these indicators.")),
-                  description = glue::glue("Verify dataset has required indicators"))
-  
-  if(selected_ind %ni% df$indicator)
     return(NULL)
   
   df_filt <- df %>%
     dplyr::filter(
       fiscal_year == max(fiscal_year),
-      country == cntry, 
-      # can be either POP_EST or PLHIV 
-      # need to figure out a way to limit user choice on this
-      # idea for 1.2:
-      # add option for both as a faceted plot
-      indicator == selected_ind)
+      country == cntry)
   
   if(nrow(df_filt) == 0)
     return(NULL)
   
   df_filt %>%
-    assertr::verify(indicator == selected_ind &
-                      fiscal_year == max(fiscal_year) &
-                      country == cntry,
+    assertr::verify(fiscal_year == max(fiscal_year) &
+                    country == cntry,
                     error_fun = err_text(glue::glue("Error: {df} has not been filtered correctly.
                                                Please check the first filter in prep_pop_pyramid().")),
                     description = glue::glue("Verify that the filters worked"))
@@ -53,12 +37,20 @@
   df_filt <- df_filt %>%
     dplyr::select(fiscal_year, country, indicator, sex, ageasentered, targets) %>%
     dplyr::group_by(fiscal_year, country, indicator, sex, ageasentered) %>%
-    dplyr::summarise(targets = sum(targets, na.rm = TRUE),
-                     .groups = "drop") %>%
-    dplyr::mutate(population = if_else(sex == "Male", -targets, targets))
+    dplyr::summarise(targets = sum(targets, na.rm = TRUE), .groups = "drop") %>%
+    dplyr::mutate(population = if_else(sex == "Male", -targets, targets)) %>%
+    dplyr::filter(indicator %in% c("POP_EST", "PLHIV"))
   
-  unknown <- df_filt  %>%
-    dplyr::filter(is.na(sex) & is.na(ageasentered)) %>%
+  plhiv_unknown <- df_filt  %>%
+    dplyr::filter(
+      indicator == "PLHIV", 
+      is.na(sex) & is.na(ageasentered)) %>%
+    dplyr::mutate(n_unknown = comma(targets))
+  
+  popest_unknown <- df_filt  %>%
+    dplyr::filter(
+      indicator == "POP_EST", 
+      is.na(sex) & is.na(ageasentered)) %>%
     dplyr::mutate(n_unknown = comma(targets))
   
   df_viz <- df_filt %>%
@@ -68,12 +60,29 @@
     # so that the note on the viz reads:
     # "Note: There are no PLHIV/POP_EST with unreported age and sex data.
     # otherwise it will show the number
-    full_join(., unknown,
+    full_join(., plhiv_unknown,
               by = join_by(fiscal_year, country, indicator, sex, ageasentered, targets,
                            population)) %>%
-    mutate(n_unknown = if_else(is.na(n_unknown) == TRUE,
-                               "no", n_unknown))
-  
+    full_join(., popest_unknown,
+              by = join_by(fiscal_year, country, indicator, sex, ageasentered, targets,
+                           population, n_unknown)) %>%
+    mutate(plhiv_unknown_sentence = case_when(
+                                  (indicator == "PLHIV" & is.na(n_unknown) == TRUE)
+                                   ~glue::glue("There are no unreported age and sex estimates for the Populaton Living with HIV indicator."), 
+                                  
+                                  (indicator == "PLHIV" & is.na(n_unknown) == FALSE)
+                                   ~glue::glue("There are {n_unknown} unreported age and sex estimates for the Populaton Living with HIV indicator.")),
+           
+           popest_unknown_sentence = case_when(
+                                  (indicator == "POP_EST" & is.na(n_unknown) == TRUE)
+                                   ~ glue::glue("There are no unreported age and sex estimates for the Populaton (Estimated) indicator."), 
+                                  (indicator == "POP_EST" & is.na(n_unknown) == FALSE)
+                                   ~ glue::glue("There are {n_unknown} unreported age and sex estimates for the Populaton (Estimated) indicator.")),
+           plhiv_unknown_sentence = replace_na(plhiv_unknown_sentence, popest_unknown_sentence[1]), 
+           popest_unknown_sentence = replace_na(popest_unknown_sentence, plhiv_unknown_sentence[1]), 
+           unknown_sentences = glue::glue("{plhiv_unknown_sentence} {popest_unknown_sentence}"),
+           indicator_label = if_else(indicator == "POP_EST", "Population (Estimated)", 
+                                     "Population Living with HIV"))
   return(df_viz)
   
 }
@@ -88,19 +97,7 @@
     return(print(paste("No data available.")))
   
   ref_id <- "aa8bd5b4"
-  vrsn <- 1.1
-  indicator <- unique(df$indicator)[1]
-  
-  # pull in the number of PLHIV/POP_EST reported with no age or sex data available
-  n_PPL_unknown <- df$n_unknown[1]
-  
-  # format the number with a comma if it is not "no"
-  if(n_PPL_unknown != "no"){
-    
-    scales::comma(n_PPL_unknown)
-    
-    return(n_PPL_unknown)
-  }
+  vrsn <- 1.2
   
   df %>%
     ggplot2::ggplot(aes(population, ageasentered, fill = sex)) +
@@ -108,16 +105,18 @@
     ggplot2::geom_vline(aes(xintercept = 0), color = "white", linewidth = 1.1)+
     ggplot2::scale_fill_manual(values = c("Male" = glitr::genoa, 
                                           "Female" = glitr::moody_blue)) +
+    facet_wrap(~indicator_label, scales = "free_x") +
     ggplot2::scale_x_continuous(
-      limits = c(-max(df$targets), max(df$targets)),
-      labels = function(x) {glue("{label_number(scale_cut = cut_short_scale())(abs(x))}")}, 
-    ) +
-    ggplot2::labs(title = glue("{unique(df$country)} - {unique(df$fiscal_year)} {indicator} Pyramid") %>% toupper,
-                  subtitle =  glue::glue("Comparison between <span style='color:{genoa}'>Males</span> & <span style='color:{moody_blue}'>FEMALES</span> {indicator} by age band"),
+      # while it's nice to have a consistent access, and this may work for 
+      # OUs with larger programs, it doesn't work for OUs with smaller programs
+     #limits = c(-max(df$targets), max(df$targets)),
+      labels = function(x) {glue("{label_number(scale_cut = cut_short_scale())(abs(x))}")}) +
+    ggplot2::labs(title = glue("{unique(df$country)} - {unique(df$fiscal_year)} {df$indicator_label} Pyramid") %>% toupper,
+                  subtitle =  glue::glue("Comparison between <span style='color:{genoa}'>Males</span> & <span style='color:{moody_blue}'>FEMALES</span> by age band"),
                   x = NULL, y = NULL, fill = NULL,
                   caption = 
-                    glue("Note: There are {n_PPL_unknown} {indicator} with unreported age and sex data.
-                  {metadata_natsubnat$caption} | USAID/OHA/SIEI |  Ref id: {ref_id} v{vrsn}")) +
+                    glue("{df$unknown_sentences[1]}
+                         {metadata_natsubnat$caption} | USAID/OHA/SIEI |  Ref id: {ref_id} v{vrsn}")) +
     glitr::si_style_yline() +
     ggplot2::theme(
       legend.position = "none",
@@ -125,13 +124,12 @@
   
   }
   
-# Example 
-  
-# df_zmb <- prep_pop_pyramid(df_natsubnat, "Zambia", "PLHIV")
+# # Example 
 # 
-# viz_pop_pyramid(df_zmb)
-  
-# df_zmb <- prep_pop_pyramid(df_natsubnat, "Zambia", "POP_EST")
+# # small program OU
+# df_ssd <- prep_pop_pyramid(df_natsubnat, "South Sudan")
+# viz_pop_pyramid(df_ssd)
 # 
-# viz_pop_pyramid(df_zmb)
-
+# # larger program OU
+# df_mwi <- prep_pop_pyramid(df_natsubnat, "Malawi")
+# viz_pop_pyramid(df_mwi)
